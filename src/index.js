5ab6a67315d4d41d2eb789bbbffbd820a1f635c6
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-02-03 11:23:00 -0300

    Alteração index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-const express = require('express')
-const { queryPromise } = require('./queryPromise')
+const express = require("express");
+const { queryPromise } = require("./queryPromise");
 
 async function createApp() {
-  const app = express()
+  const app = express();
   const sqlTable = `CREATE TABLE IF NOT EXISTS characterSW(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))`;
 
-  await queryPromise.query(sqlTable)
+  await queryPromise.query(sqlTable);
 
-  const characters = [['Luke Skywalker'], ['Darth Vader'], ['Han Solo'], ['Chewbacca'], ['Leia Organa'], ['Obi-Wan Kenobi'], ['Yoda'], ['R2-D2'], ['C-3PO']]
+  const characters = [
+    ["Luke Skywalker"],
+    ["Darth Vader"],
+    ["Han Solo"],
+    ["Chewbacca"],
+    ["Leia Organa"],
+    ["Obi-Wan Kenobi"],
+    ["Yoda"],
+    ["R2-D2"],
+    ["C-3PO"],
+  ];
   const sqlInsert = `INSERT INTO characterSW(name) VALUES ?`;
 
-  await queryPromise.queryMultiple(sqlInsert, characters)
+  await queryPromise.queryMultiple(sqlInsert, characters);
 
-  app.get('/', async (req, res) => {
-    const selectCharacters = `SELECT * FROM characterSW`
-    const allCharacters = await queryPromise.query(selectCharacters)
+  app.get("/", async (req, res) => {
+    const selectCharacters = `SELECT * FROM characterSW`;
+    const allCharacters = await queryPromise.query(selectCharacters);
 
-    const html = `<h1>Star Wars Characters</h1>\n
+    const html = `
+    <h1>Full Cycle Rocks!</h1>
+    <h3>Star Wars Characters</h3>\n
   <ul>
-    ${allCharacters.map(character => `<li>${character.name}</li>`).join('')}
-  </ul>`
+    ${allCharacters.map((character) => `<li>${character.name}</li>`).join("")}
+  </ul>`;
 
-    res.send(html)
-  })
-  return app
+    res.send(html);
+  });
+  return app;
 }
 
-module.exports = createApp
+module.exports = createApp;
